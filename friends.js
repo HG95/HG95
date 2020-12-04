e8600816c84b52072f2b48d29461ccf51332b967
171558383@qq.com <1092090447@qq.com>
2020-12-04 18:43:03 +0800

    Site updated: 2020-12-04 18:42:59

--- friends.js
+++ friends.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>link = {
+link = {
     init:function() {
         const that=this;
         $.getJSON("/links.json",
@@ -8,27 +8,14 @@
     },
     render:function(data){
         let name,site,info,avatar,li="";
-        for(let i=0;i<data.length;i++){ name="data[i].name;" site="data[i].site;" info="data[i].info;" avatar="data[i].avatar;" li+="<div class=&quot;card&quot;>" +'<img="" class="ava" src="'+avatar+'"><div class="card-header"><div><a href="'+site+'" target="_blank">'+name+'</a></div><div class="info">'+info+'</div></div>';
+        for(let i=0;i<data.length;i++){
+            name=data[i].name;
+            site=data[i].site;
+            info=data[i].info;
+            avatar=data[i].avatar;
+            li+='<div class="card">'+'<img class="ava" src="'+avatar+'"/><div class="card-header"><div><a href="'+site+'" target="_blank">'+name+'</a></div><div class="info">'+info+'</div></div></div>';
         }
         $(".link-navigation").append(li);
     }
 };
-link.init();</data.length;i++){><script>
-        document.querySelectorAll('.github-emoji')
-          .forEach(el => {
-            if (!el.dataset.src) { return; }
-            const img = document.createElement('img');
-            img.style = 'display:none !important;';
-            img.src = el.dataset.src;
-            img.addEventListener('error', () => {
-              img.remove();
-              el.style.color = 'inherit';
-              el.style.backgroundImage = 'none';
-              el.style.background = 'none';
-            });
-            img.addEventListener('load', () => {
-              img.remove();
-            });
-            document.body.appendChild(img);
-          });
-      </script>
+link.init();
